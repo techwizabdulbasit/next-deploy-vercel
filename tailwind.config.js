3f170cf9efd8f73d4e21d58686142b084f1fb7fa
techwizabdulbasit <134509392+techwizabdulbasit@users.noreply.github.com>
2023-05-24 17:44:02 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{js,ts,jsx,tsx,mdx,html}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
