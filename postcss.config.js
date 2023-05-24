06aa3329e56bfbe57457b59323985b0de530616d
techwizabdulbasit <wizardtechab@gmail.com>
2023-05-24 19:50:24 +0500

    rentzo 24 may

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 3f170cf9efd8f73d4e21d58686142b084f1fb7fa
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,18 +10,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-=======
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./**/*.{js,ts,jsx,tsx,mdx,html}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
->>>>>>> 3f170cf9efd8f73d4e21d58686142b084f1fb7fa
 }
